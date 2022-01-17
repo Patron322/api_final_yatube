b9bdb31a18f5b2facb5117d9e2b825426ce205e2
Алексей Бахмутов <ququ.kuku@yandex.ru>
2022-01-17 17:18:33 +0500

    v1.0

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,63 @@
+from rest_framework import serializers
+from rest_framework.relations import SlugRelatedField
+from django.contrib.auth import get_user_model
+from rest_framework.validators import UniqueTogetherValidator
+
+from posts.models import Comment, Post, Group, Follow
+
+User = get_user_model()
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        fields = '__all__'
+        model = Post
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username'
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all(),
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all(),
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Follow
+        validators = [UniqueTogetherValidator(
+            queryset=Follow.objects.all(), fields=['user', 'following'],
+            message='Невозможно подписаться на автора повторно'
+        )]
+
+    def validate(self, data):
+        if (
+            self.context['request'].method == 'POST'
+            and data['user'] == data['following']
+        ):
+            raise serializers.ValidationError(
+                'Невозможно подписаться на самого себя'
+            )
+        return data
